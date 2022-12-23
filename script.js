d427209d9f2caf3d2710c84b87e05ea737ffaa87
Yara M Esmaiel <50365379+yara-esmaiel@users.noreply.github.com>
2022-12-23 10:50:13 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+  $( function() {
+    $( "#iputChechIn" ).datepicker();
+    $( "#iputChechOut" ).datepicker();
+    $('.level-slider').slick({
+      dots: true,
+      slidesToShow: 2,
+      slidesToScroll: 1,
+      arrows: false,
+    });
+  } );
